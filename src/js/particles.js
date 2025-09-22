90775588a26457ca27ca3965c983cf415a7d80d0
André Loureiro <andrexprofissional@gmail.com>
2025-09-22 14:46:09 +0100

    upload do meu projeto de portfolio para o Github

---
+++ src/js/particles.js
@@ -0,0 +1,14 @@
+particlesJS("particles-js", {
+  particles: {
+    number: { value: 100 },
+    color: { value: "#ffffff" },
+    shape: { type: "circle" },
+    opacity: { value: 0.8 },
+    size: { value: 2 },
+    line_linked: { enable: false },
+    move: { enable: true, speed: 0.5 },
+  },
+  interactivity: {
+    events: { onhover: { enable: false }, onclick: { enable: false } },
+  },
+});
